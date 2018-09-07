dad3f354149e4d1f8ec06a8f7aa86866b046eccb
Stephen Courtier <steves0089@gmail.com>
2018-09-07 16:22:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!" && phrase.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   end 
 end
